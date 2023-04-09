0b3cd96b02b7a47fbd83ff677c93a94c2ada03eb
EC2 Default User <126954590+koheimuta@users.noreply.github.com>
2023-04-09 15:02:57 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  resources :books
+  get 'homes/about' => 'homes#about',as: 'about'
+  resources :users, only: [:show, :edit, :update]
 end
